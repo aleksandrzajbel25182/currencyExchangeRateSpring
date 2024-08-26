11ba9dfc8ebc5fd1a0252f503cfa07d84275cb3f
Александр Зайбель <aleksandrzajbel25182@gmail.com>
2024-08-26 10:39:31 +0700

    Create project spring

---
+++ src/main/java/com/example/currencyexchangerate/currency_exchange_rate_spring/CurrencyExchangeRateSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.currencyexchangerate.currency_exchange_rate_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyExchangeRateSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyExchangeRateSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/currencyexchangerate/currency_exchange_rate_spring/CurrencyExchangeRateSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.currencyexchangerate.currency_exchange_rate_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyExchangeRateSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
